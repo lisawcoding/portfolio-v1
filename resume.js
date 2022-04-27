958546f4a2f37917e8afe6ce5abb030b1848e57c
Lisa <sulinwood@gmail.com>
2022-04-27 11:41:11 +0800

    commit test 1

--- resume.js
+++ resume.js
@@ -403,3 +403,5 @@ document.querySelector("#topBtn").addEventListener('click',()=>{
 	document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
+
+asdf
