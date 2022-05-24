f6e5d55a1b82f41faf7f80f91017fe411fb9bb4d
Lisa <sulinwood@gmail.com>
2022-05-24 23:26:34 +0800

    change color theme

--- resume.js
+++ resume.js
@@ -403,5 +403,3 @@ document.querySelector("#topBtn").addEventListener('click',()=>{
 	document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 })
-
-asdf
